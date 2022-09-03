6517919b6d7f7b8f16969e0d7502e9db58750655
Bruno de Vasconcelos Correa <182010051@senacinformatica.br>
2022-09-02 21:26:01 -0300

    Final da Aula de JavaScript

---
+++ html/Aula 15/script.js
@@ -0,0 +1,9 @@
+function minhaFuncaoExterna(){
+	
+	n1=2;
+	const n2=3;
+	var r = n1 * n2;
+	
+	var expressao = "Mult"
+	alert("Externa\nA página está carregada.\nResultado: " + expressao + "-" + r);
+}
