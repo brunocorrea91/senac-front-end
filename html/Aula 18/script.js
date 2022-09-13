c119d0e8c6ff91211492af8049c0977e0a044896
Bruno de Vasconcelos Correa <182010051@senacinformatica.br>
2022-09-12 21:55:51 -0300

    Fim da Aula 18 - Começar arrumando a impressão final

---
+++ html/Aula 18/script.js
@@ -0,0 +1,40 @@
+function abrirCPF(){
+   campoPF.type = "text";
+   campoPJ.type = "hidden";
+}
+function abrirCNPJ(){
+    campoPJ.type = "text"; 
+    campoPF.type = "hidden";
+}
+function mostrarTudo(){
+   nome = formUser.nome.value;
+   pessoa = formUser.inserirCampoPF.value;
+   alert(nome + pessoa);
+}
+
+
+function mascaraCPF(numCPF) {
+    cpf = numCPF.value;
+    numCPF.setAttribute("maxlength", "14");
+    if (cpf.length == 3) numCPF.value = numCPF.value + ".";
+    if (cpf.length == 7) numCPF.value = numCPF.value + ".";
+    if (cpf.length == 11) numCPF.value += "-";
+
+    if (isNaN(cpf[cpf.length - 1])) {
+        numCPF.value = cpf.substring(0, numCPF.length - 1);
+        return;
+    }
+}
+function mascaraCNPJ(numCNPJ) {
+    cnpj = numCNPJ.value;
+    numCNPJ.setAttribute("maxlength", "18");
+    if (cnpj.length == 2) numCNPJ.value = numCNPJ.value + ".";
+    if (cnpj.length == 6) numCNPJ.value = numCNPJ.value + ".";
+    if (cnpj.length == 10) numCNPJ.value = numCNPJ.value + "/";
+    if (cnpj.length == 15) numCNPJ.value = numCNPJ.value + "-";
+
+    if (isNaN(cnpj[cnpj.length - 1])) {
+        numCNPJ.value = cnpj.substring(0, numCNPJ.length - 1);
+        return;
+    }
+}
