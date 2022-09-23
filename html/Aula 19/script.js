cfe138ad3dad39b6837cdfceea12a6828a20c92e
Bruno de Vasconcelos Correa <182010051@senacinformatica.br>
2022-09-22 21:25:18 -0300

    final do exercício Mario

--- html/Aula 19/script.js
+++ html/Aula 19/script.js
@@ -1,16 +1,31 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const clouds = document.querySelector(".clouds");
+const gameOver = new Audio ('./audio/gameover.mp3');
+const jumpMove = new Audio ('./audio/jump.mp3');
+const startPlay = new Audio ('./audio/play.mp3');
 let score = 0;
 let pipePositionScore = pipe.offsetLeft;
 
 
 const jump = () =>{
+    
     mario.classList.add("jump");
+    jumpMove.play();
     setTimeout(()=>{
         mario.classList.remove("jump");
     }, 500);
 }
+
+function iniciar(){
+    document.addEventListener("keydown", jump); 
+    pipe.classList.add("pipe-animation");
+    clouds.classList.add("clouds-animation");
+    document.getElementById("iniciar").setAttribute("hidden", "hidden");
+    startPlay.play();
+}
+
+
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
@@ -18,6 +33,10 @@ const loop = setInterval(() => {
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
     const cloudsPosition = clouds.offsetLeft;
     const recomecar = document.getElementById("recomecar");
+    
+
+    
+    
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
@@ -26,7 +45,10 @@ const loop = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`;
         mario.src = "./imagens/game-over.png";
         mario.style.width = "80px" ;
-        mario.style.marginLeft = "45px";    
+        mario.style.marginLeft = "45px";  
+        gameOver.play(); 
+        startPlay.pause(); 
+        jumpMove.pause();
         //
         clouds.style.animation = "none";
         clouds.style.left = `${cloudsPosition}px`;          
@@ -37,18 +59,15 @@ const loop = setInterval(() => {
     }  else if (pipePosition>pipePositionScore){
             score += 100;
             document.getElementById("score").innerHTML = score;
-         
-            
-            
-            if(score>=200){
-                //pipe.style.animation = "pipe-animation 1.5s infinite linear";  
-                pipe.style.animationDuration = "1s";  
-            }
-            
-                    
+            pipe.style.animationDuration = aceleraPipe(); 
     }
     pipePositionScore = pipePosition;
+    console.log(pipePositionScore);
 },10);
 
+function aceleraPipe(){
+    let pipeT = +window.getComputedStyle(pipe).animationDuration.replace("s","");
+    pipeT -= 0.2;
+    return pipeT + "s";
+}
 
-document.addEventListener("keydown", jump); 
